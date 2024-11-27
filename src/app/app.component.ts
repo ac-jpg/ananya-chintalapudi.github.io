b065fe0e6d06c4b08757853b0f40b3c794c3d1ba
ananya-chintalapudi <ananya.cx@gmail.com>
2024-11-27 12:16:20 -0600

    new website

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faMountain } from '@fortawesome/free-solid-svg-icons';
+import { faCar } from '@fortawesome/free-solid-svg-icons';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'portfolio';
+
+  toggleDetails(orgId: string): void {
+    const orgCard = document.getElementById(orgId)?.parentElement;
+    const isOpen = orgCard?.classList.contains('open');
+    
+    document.querySelectorAll('.org-card').forEach((card) => card.classList.remove('open'));
+    
+    if (!isOpen) {
+      orgCard?.classList.add('open');
+    }
+    
+  }
+
+  faMountain = faMountain;
+  faCar = faCar;
+  faCoffee = faCoffee;
+
 }
